cc76ed1fd27da24d779fb56669bec6cfdb69d240
bernardo <bernardo_hc@hotmail.com.br>
2023-07-25 09:25:56 -0300

    Pesquisa de Satisfacao

--- GTS Homologação 1/forms/IM - Pesquia de Satisfação/events/atividades.js
+++ GTS Homologação 1/forms/IM - Pesquia de Satisfação/events/atividades.js
@@ -1,6 +1,5 @@
 var INICIO_0 = 0;
 var INICIO = 4;
-var Inicio_da_Pesquisa = 5;
 var Registro_Ocorrências = 18;
 var Pesquisa_Pos_Safra = 10;
 var Ocorrência_Pos_Safra = 26;
--- GTS Homologação 1/forms/IM - Pesquia de Satisfação/events/displayFields.js
+++ GTS Homologação 1/forms/IM - Pesquia de Satisfação/events/displayFields.js
@@ -12,37 +12,64 @@ function displayFields(form, customHTML) {
 	 */
 
 	if(atv_atual == INICIO_0) {
-		form.setValue("solUsuarioFluig",  usuarioCorrente.getCode() );
+		controleAbas(form, customHTML, 'indisponivel');
 
+		form.setValue("pesNomePesquisador",  usuarioCorrente.getFullName() );
+		form.setValue('processoId', getValue('WKNumProces'));
+		form.setValue("solUsuarioFluig",  usuarioCorrente.getCode() );
+		//Oculta divs ocorrencias
 		form.setVisibleById("divPesqOcorrencia", false);
-        form.setVisibleById("divPesqTecOcorrencia", false);
+		form.setVisibleById("divFimOcorrencia", false);
+        form.setVisibleById("divPsPesqOcorrencia", false);
+        form.setVisibleById("divPsFimOcorrencia", false);
 	}else if (atv_atual == INICIO) {
 		if (form.getFormMode() == 'MOD') {
-
+			form.setVisibleById("divPesqOcorrencia", false);
+			form.setVisibleById("divFimOcorrencia", false);
+        	form.setVisibleById("divPsPesqOcorrencia", false);
+        	form.setVisibleById("divPsFimOcorrencia", false);
 		}
 
-	}else if (atv_atual == Inicio_da_Pesquisa) {
+	}else if (atv_atual == Registro_Ocorrências) {
 		if (form.getFormMode() == 'MOD') {
-
-		}
-
-	}	else if (atv_atual == Registro_Ocorrências) {
-		if (form.getFormMode() == 'MOD') {
+			controleAbas(form, customHTML, 'indisponivel');
 
 		}
 
 	}else if (atv_atual == Pesquisa_Pos_Safra) {
 		if (form.getFormMode() == 'MOD') {
 
+			controleAbas(form, customHTML, 'disponivel');
+
+			form.setVisibleById("divFimOcorrencia", false);
+        	form.setVisibleById("divPsPesqOcorrencia", false);
+        	form.setVisibleById("divPsFimOcorrencia", false);
+        	form.setVisibleById("divAddOcorrencia", false);
 		}
 
-	}	else if (atv_atual == Ocorrência_Pos_Safra) {
+	}else if (atv_atual == Ocorrência_Pos_Safra) {
 		if (form.getFormMode() == 'MOD') {
+			controleAbas(form, customHTML, 'disponivel');
 
+			form.setVisibleById("divFimOcorrencia", false);
+			form.setVisibleById("divAddOcorrencia", false);
 		}
 
+	}else if (atv_atual == FIM) {
+		controleAbas(form, customHTML, 'disponivel');
+		
+		form.setVisibleById("divAddOcorrencia", false);
+		form.setVisibleById("divAddOcorrenciaPs", false);		
 	}
+}
 
+function controleAbas(form, customHTML, tabNavegacao){
+	
+	
+	if(tabNavegacao == 'disponivel'){
+		customHTML.append("<script>$('#tabPesquisaPosSfra').removeClass('disabled');</script>");
+		customHTML.append("<script>$('#tabPesquisaPosSfra a').prop('href', '#divPesquisaPosSafra');</script>");
+	}
 
 }
 
