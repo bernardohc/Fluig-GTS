9e5c585990b9563e6e8261a9f755c0432d2fe881
Bernardo <bernardo.correa@gtsdobrasil.com.br>
2024-05-14 09:41:56 -0300

    nota ent tec

--- GTS Produção/datasets/Produção/dsNotaPesquisaDeSatisfacao.js
+++ GTS Produção/datasets/Produção/dsNotaPesquisaDeSatisfacao.js
@@ -16,7 +16,7 @@ function createDataset(fields, constraints, sortFields) {
 	dataset.addColumn("MSGRET");
 	
 	//Cabeçalho
-	var numSerie  = "";
+	var numSerie  = "FPC0821410108";
 
 	var constraintsForm = new Array(); 
 	for (var i in constraints){
@@ -27,15 +27,15 @@ function createDataset(fields, constraints, sortFields) {
 	}
 	if( numSerie != ""  ){
 		var SQL =	" SELECT pesqNotaAtendimento ,pesquisa.pesqNumSerie " +
-					" FROM ML001097 pesquisa (NOLOCK)" +
+					" FROM ML001046 pesquisa (NOLOCK)" +
 					" JOIN PROCES_WORKFLOW" +
 					" 	ON pesquisa.companyid = PROCES_WORKFLOW.COD_EMPRESA" +
 					" 	AND pesquisa.cardid = PROCES_WORKFLOW.NR_DOCUMENTO_CARD_INDEX" +
 					" 	AND pesquisa.documentid = PROCES_WORKFLOW.NR_DOCUMENTO_CARD" +
-					" 	AND pesquisa.version = (SELECT max(version) FROM ML001097 entrega_SUB WHERE entrega_SUB.documentid = pesquisa.documentid )" +
+					" 	AND pesquisa.version = (SELECT max(version) FROM ML001046 entrega_SUB WHERE entrega_SUB.documentid = pesquisa.documentid )" +
 					" INNER JOIN (" +
 					" 	SELECT max(START_DATE) as START_DATE,pesqNumSerie" +
-					" 	FROM ML001097 pesquisa (NOLOCK)" +
+					" 	FROM ML001046 pesquisa (NOLOCK)" +
 					" 	JOIN PROCES_WORKFLOW" +
 					" 	ON pesquisa.companyid = PROCES_WORKFLOW.COD_EMPRESA" +
 					" 	AND pesquisa.cardid = PROCES_WORKFLOW.NR_DOCUMENTO_CARD_INDEX" +
@@ -43,21 +43,15 @@ function createDataset(fields, constraints, sortFields) {
 					" 	PROCES_WORKFLOW.COD_DEF_PROCES = 'IM - Pesquisa de Satisfação'" +
 					" 	AND STATUS <> 1" +
 					" 	AND pesquisa.version =" +
-					" 	(SELECT max(version) FROM ML001097 MLTB WHERE MLTB.documentid = pesquisa.documentid)" +
+					" 	(SELECT max(version) FROM ML001046 MLTB WHERE MLTB.documentid = pesquisa.documentid)" +
 					" 	group by pesqNumSerie" +
 					" 	)  as sub on sub.pesqNumSerie = pesquisa.pesqNumSerie and PROCES_WORKFLOW.START_DATE = sub.START_DATE" +
 					" 	where pesquisa.pesqNumSerie = '" + numSerie + "'" ;
 	}
-
+	//Tabela Produção ML001046
+	//Tabela Homologação ML001097
 		
-	// var SQL =	" SELECT pesqNotaAtendimento ,pesqNumSerie " +
-	// 			" FROM ML001097 entrega (NOLOCK) " ;
-				
-	// 	if( numSerie != ""  ){
-	// 		SQL += " WHERE pesqNumSerie = '" + numSerie + "'";
-	// 	}
-		
-	log.info(" dsConsultaSAC:"+ SQL);	
+	log.info(" dsConsultaNota"+ SQL);	
 	
 	var statementWD = connectionWD.prepareStatement(SQL);
 	var rsWD = statementWD.executeQuery();
