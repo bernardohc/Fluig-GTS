90837eac73a5a4584e19f0a9f50785bc5eae09e1
Bernardo <bernardo.correa@gtsdobrasil.com.br>
2024-05-27 11:27:29 -0300

    att menu qualidade sgq

--- GTS Homologação 1/wcm/widget/WSRestOAuth/src/main/resources/view.ftl
+++
@@ -1,4 +0,0 @@
-<div id="WSRestOAuth_${instanceId}" class="super-widget wcm-widget-class fluig-style-guide" data-params="WSRestOAuth.instance()">
-
-</div>
-
--- GTS Homologação 1/wcm/widget/rest-oauth/src/main/java/com/fluig/config/ApplicationConfig.java
+++
@@ -1,9 +0,0 @@
-package com.fluig.config;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/rest")
-public class ApplicationConfig extends Application {
-	// nao e necessario codigo, precisamos apenas para o @ApplicationPath
-}
